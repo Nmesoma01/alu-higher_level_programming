88e37fac50f5185cc9f9d7fb25e22604d2cd14ef
Nmesoma01 <n.jeff-nwankwo@alustudent.com>
2022-07-13 10:53:56 -0700

    my commit

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_1 = []
-    for x in my_list:
-        if x == search:
-            new_1.append(replace)
-        else:
-            new_1.append(x)
-    return
+    new_list = list(map(lambda x: replace if x == search else x, my_list))
+    return (new_list)
