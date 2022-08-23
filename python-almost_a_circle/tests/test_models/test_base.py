0999b034a5baed61c3882bbe8574b5398476b2c6
Nmesoma01 <n.jeff-nwankwo@alustudent.com>
2022-08-23 10:29:20 -0700

    circle

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,6 +1,15 @@
-
 #!/usr/bin/python3
-"""Defines unittests for base.py
+# test_base.py
+"""Defines unittests for base.py.
+Unittest classes:
+    TestBase_instantiation - line 23
+    TestBase_to_json_string - line 110
+    TestBase_save_to_file - line 156
+    TestBase_from_json_string - line 234
+    TestBase_create - line 288
+    TestBase_load_from_file - line 340
+    TestBase_save_to_file_csv - line 406
+    TestBase_load_from_file_csv - line 484
 """
 import os
 import unittest
@@ -392,5 +401,5 @@ class TestBase_load_from_file(unittest.TestCase):
             Base.load_from_file([], 1)
 
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     unittest.main()
