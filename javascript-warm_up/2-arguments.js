d6533817308e0cd49215bb01753e1aea03f0035d
Nmesoma01 <n.jeff-nwankwo@alustudent.com>
2022-10-09 19:22:10 -0700

    java

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const args = process.argv;
-if (args.length === 1) {
+if (args.length === 3) {
   console.log('Argument found');
-} else if (args.length = null) {
-  console.log('No argument');
-} else {
+} else if (args.length > 3) {
   console.log('Arguments found');
+} else {
+  console.log('No argument');
 }
